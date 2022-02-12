ce49e7f8d53d442e4e3f34b7a9535a0840bb36bc
rayansami <rayansami007@gmail.com>
2022-02-11 21:29:25 -0500

    updating data preprocessing directory

--- Data Preprocessing/csv-downsampler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=downsamp
-#SBATCH --cpus-per-task 20
-#SBATCH --mail-type=all          # send email on job start, end and fail
-#SBATCH --mail-user=muddi004@odu.edu
-
-enable_lmod
-module load container_env pytorch-gpu/1.9.0
-crun -p ~/envs/citationparser python csv-downsampler.py
